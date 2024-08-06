d57b5132e987d8f7e162ece7eedcfccaa1f47659
jcarranzan <jcarranz@redhat.com>
2024-08-06 12:37:06 +0200

    trye to fix the assertions

--- quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliConfigEncryptIT.java
+++ quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliConfigEncryptIT.java
@@ -63,9 +63,14 @@ public class QuarkusCliConfigEncryptIT {
                 .secretConsumer(Assertions::assertNotNull)
                 .storeSecretAsSecretExpression(SECRET_1.propertyName)
                 .generatedKeyConsumer(encKey -> encryptionKey = encKey)
-                .assertCommandOutputContains("""
-                        The secret %s was encrypted to
-                        """.formatted(SECRET_1.secret))
+                /*
+                 * .assertCommandOutputContains("""
+                 * The secret %s was encrypted to
+                 * """.formatted(SECRET_1.secret))
+                 */
+                .assertCommandOutputContains(String.format(
+                        "The secret %s was encrypted to",
+                        EncryptPropertyTest.escapeArgumentForWindows(SECRET_1.secret)))
                 .assertCommandOutputContains("""
                         with the generated encryption key (base64):
                         """);
