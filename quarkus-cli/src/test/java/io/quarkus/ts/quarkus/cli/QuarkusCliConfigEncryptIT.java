538c4dcd7aa00a239485adea831e42871766a24b
jcarranzan <jcarranz@redhat.com>
2024-08-07 15:10:45 +0200

    add double quotes

--- quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliConfigEncryptIT.java
+++ quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliConfigEncryptIT.java
@@ -64,7 +64,7 @@ public class QuarkusCliConfigEncryptIT {
                 .storeSecretAsSecretExpression(SECRET_1.propertyName)
                 .generatedKeyConsumer(encKey -> encryptionKey = encKey)
                 .assertCommandOutputContains("""
-                        The secret %s was encrypted to
+                        The secret "%s" was encrypted to
                         """.formatted(SECRET_1.secret))
                 .assertCommandOutputContains("""
                         with the generated encryption key (base64):
